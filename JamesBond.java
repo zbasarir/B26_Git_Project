3ff5680bc1002c35e8cc0c965de80f25cee6d9ba
zbasarir <98862839+zbasarir@users.noreply.github.com>
2022-03-26 16:36:35 -0400

    Update JamesBond.java

--- JamesBond.java
+++ JamesBond.java
@@ -2,7 +2,7 @@ package day0;
 
 public class JamesBond {
   
-  public static main (String [] args){
+  public static void main (String [] args){
   System.out.println("James Bond hacked the project");  
 }
 
