bdf32c53bebf4bd8718ccfb7c0f0c13981f58d22
zbasarir <ziynetyapar@gmail.com>
2022-03-26 16:13:00 -0400

    wrote the code for First Class

--- src/day0/First.java
+++ src/day0/First.java
@@ -4,6 +4,11 @@ public class First {
     //TODO: Work on this tomorrow
     // add some cool code
 
+    public static void main(String[] args) {
+        System.out.println("java");
+        System.out.println("Working...");
+    }
+
     // extra line1
     // extra line2
     // extra line3
